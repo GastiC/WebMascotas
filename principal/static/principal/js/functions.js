93d6cb828bbe5c49627a23164f3625635ad5f8de
Gasti Colaneri <colaneri.gaston@hotmail.com>
2020-11-06 22:29:02 -0300

    Prueba para chequear el deployment en PythonAnywhere 2

--- principal/static/principal/js/functions.js
+++ principal/static/principal/js/functions.js
@@ -6,8 +6,8 @@ function formulario_contacto(e) {
     for (let i = 0; i < elementos.length; i++) {
         const element = elementos[i].value;
         console.log(element);
-        alert("Muchas gracias por contactarte con nosotros")
     }
+    alert("Muchas gracias por contactarte con nosotros")
 }
 
 formulario.addEventListener("submit", formulario_contacto)
