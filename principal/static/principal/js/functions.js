43c7f9be461a41893d6ab13cb6c3fd6cffa38f5f
Gasti Colaneri <colaneri.gaston@hotmail.com>
2021-02-06 01:13:20 -0300

    Cambios estéticos, sumé Sweet Alert pero no estaría funcionando muy bien

--- principal/static/principal/js/functions.js
+++ principal/static/principal/js/functions.js
@@ -1,17 +1,32 @@
 const expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
 
-function newsletter(e){
+function newsletter(){
     var email = document.getElementById("id_email_newsletter").value;
     if (expresion.test(email)){
-        console.log(document.getElementById("id_email_newsletter").value);
-        alert("Muchas gracias por suscribirte. En unos momentos recibirás un correo nuestro");}
+        Swal.fire({
+            title: 'Newsletter',
+            text: 'Muchas gracias por suscribirte. En unos momentos recibirás un correo nuestro',
+            icon: 'success',
+            confirmButtonText: 'Listo',
+          })
+          .then((result) => {
+            if (result.value) {
+              $("#id_newsletter").submit();
+            }
+          })
+        }
     else {
-        console.log(document.getElementById("id_email_newsletter").value);
-    }
-}
+        Swal.fire({
+            title: 'Newsletter',
+            text: 'Ingrese un correo electrónico válido',
+            icon: 'error',
+            confirmButtonText: 'Ok',
+          })
+        }    
+};
 
 
-function contacto(e){
+/*function contacto(e){
     var email = document.getElementById("id_email").value,
     nombre = document.getElementById("id_nombre").value,
     apellido = document.getElementById("id_apellido").value,
@@ -30,7 +45,39 @@ function contacto(e){
     else {
         console.log(document.getElementById("id_email").value);
     }
+};*/
+function contacto(){
+    var email = document.getElementById("id_email").value,
+    nombre = document.getElementById("id_nombre").value,
+    apellido = document.getElementById("id_apellido").value,
+    emailConf = document.getElementById("id_confirmacionEmail").value,
+    prefijo = document.getElementById("id_codigoArea").value,
+    telefono = document.getElementById("id_telefono").value,
+    telefonoAlt = document.getElementById("id_celular").value,
+    domicilio = document.getElementById("id_domicilio").value,
+    localidad = document.getElementById("id_localidad").value,
+    cp = document.getElementById("id_cp").value,
+    consulta = document.getElementById("id_consulta").value;
+
+    if (nombre != "" && apellido != "" && expresion.test(email) && expresion.test(emailConf) && prefijo != NaN && telefono != NaN && telefonoAlt != NaN && domicilio != "" && localidad != "" && cp != "" && consulta != ""){
+        Swal.fire({
+            title: 'Newsletter',
+            text: 'Muchas gracias por suscribirte. En unos momentos recibirás un correo nuestro',
+            icon: 'success',
+            confirmButtonText: 'Listo',
+        })
+        .then((result) => {
+            if (result.value) {
+            $("#formulario_contacto").submit();
+            }
+        })
+        }
+    else {
+        $("#formulario_contacto").submit();
+    }
 };
+
+
 
 function descripcion_busqueda(){
     elemento_descripcion = document.getElementById('id_descripcion').value
@@ -52,10 +99,12 @@ function descripcion_refugio(){
 function onresize_body () {
     var foto = document.getElementsByClassName("imagen_banner")[0];
     var fondo = document.getElementsByClassName("fondo_banner")[0];
-
-    if (window.innerWidth > 575) {
+    576
+    if (window.innerWidth <= 575) {
+        fondo.style.minHeight = 0;
+    } else if (window.innerWidth >=  576) {
         total = foto.clientHeight
-        fondo.style.minHeight = total + "px";
-    } else { fondo.style.minHeight = 0
-     };
+        fondo.style.minHeight = total +"px";
+    } 
+
 };
