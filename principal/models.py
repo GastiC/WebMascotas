b1a3c244df746486c0720e139d1c463f6aaa3ffe
Gasti Colaneri <colaneri.gaston@hotmail.com>
2021-05-04 00:35:33 -0300

    Cambios en el formulario contacto

--- principal/models.py
+++ principal/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Contacto(models.Model):
     nombre = models.CharField(max_length=50) 
     apellido = models.CharField(max_length=50) 
